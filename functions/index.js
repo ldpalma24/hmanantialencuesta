7e8e136df4d4ddbf4bc219f4e988d6dfacf595e4
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-15 09:39:04 -0400

    nuevo

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exceljs = require('exceljs');
-const cors = require('cors'); 
+const cors = require('cors');
+const fs = require('fs');
 const app = express();
 
 app.use(cors());
@@ -10,34 +11,50 @@ app.use(bodyParser.json());
 app.post('/submit-survey', async (req, res) => {
   try {
     const data = req.body;
-
     console.log('Datos recibidos:', data);
+
     if (Object.keys(data).length === 0) {
       console.error('No se recibieron datos.');
       res.status(400).json({ message: 'No se recibieron datos.' });
       return;
     }
 
-    const workbook = new exceljs.Workbook();
-    const worksheet = workbook.addWorksheet('Survey Data');
+    const filePath = 'data/survey_data.xlsx';
+    let workbook;
+    const exists = fs.existsSync(filePath);
 
-    worksheet.columns = [
-      { header: 'Nombre', key: 'nombre' },
-      { header: 'Habitación', key: 'habitacion' },
-      // otros encabezados
-    ];
+    if (exists) {
+      // Cargar el archivo existente
+      workbook = new exceljs.Workbook();
+      await workbook.xlsx.readFile(filePath);
+    } else {
+      // Crear un nuevo archivo
+      workbook = new exceljs.Workbook();
+      workbook.addWorksheet('Survey Data');
+    }
+
+    const worksheet = workbook.getWorksheet('Survey Data');
+
+    // Definir las columnas solo si el archivo es nuevo
+    if (!exists) {
+      worksheet.columns = [
+        { header: 'Nombre', key: 'nombre' },
+        { header: 'Habitación', key: 'habitacion' },
+        // otros encabezados
+      ];
+    }
 
     worksheet.addRow(data);
     console.log('Datos añadidos al worksheet:', data);
 
-    await workbook.xlsx.writeFile('data/survey_data.xlsx');
+    await workbook.xlsx.writeFile(filePath);
     console.log('Archivo guardado en data/survey_data.xlsx');
     res.json({ message: 'Survey submitted and data exported to Excel' });
 
     const { exec } = require('child_process');
     exec('git add data/survey_data.xlsx && git commit -m "Update survey data" && git push', (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error executing command: ${error}`);
+        console.error(`Error ejecutando el comando: ${error}`);
         return;
       }
       console.log(`stdout: ${stdout}`);
