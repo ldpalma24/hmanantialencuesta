a38a8a9faf0822b2fa2fc17a60ac5dd2a241ec9e
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-14 23:42:38 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exceljs = require('exceljs');
-const cors = require('cors'); // Añadimos el paquete cors
+const cors = require('cors'); 
 const app = express();
 
-app.use(cors()); // Usamos cors para permitir solicitudes de origen cruzado
+app.use(cors()); 
 app.use(bodyParser.json());
 
 app.post('/submit-survey', async (req, res) => {
   try {
     const data = req.body;
 
-    // Verifica que los datos se reciban correctamente
     console.log('Datos recibidos:', data);
     if (Object.keys(data).length === 0) {
       console.error('No se recibieron datos.');
