5b8347d68816ddca92fac6e03338a594eb2f75ec
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-16 14:06:07 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,36 +7,24 @@ const app = express();
 const uri = "mongodb+srv://sudomanantial:<password>@hmvalencia2024.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.options('*', cors());  // Permite las solicitudes preflight de todas las rutas
-
+// Middleware CORS
 app.use(cors({
   origin: 'https://ldpalma24.github.io',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ldpalma24.github.io');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204).end();
-  }
-  next();
-});
-
 app.use(bodyParser.json());
 
-// Middleware adicional para habilitar CORS
+// Configurar cabeceras CORS en todas las respuestas
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ldpalma24.github.io');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(204).end();
+    return res.sendStatus(204);
   }
   next();
 });
@@ -70,9 +58,7 @@ client.connect(err => {
     }
   });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
 });
