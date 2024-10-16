30543f9fd4071c95819ea8f83ffb6e3975da107d
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-16 12:32:31 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -7,12 +7,25 @@ const app = express();
 const uri = "mongodb+srv://sudomanantial:<password>@hmvalencia2024.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors({ 
+app.options('*', cors());  // Permite las solicitudes preflight de todas las rutas
+
+app.use(cors({
   origin: 'https://ldpalma24.github.io',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Habilita 'Access-Control-Allow-Credentials'
+  credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ldpalma24.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204).end();
+  }
+  next();
+});
 
 app.use(bodyParser.json());
 
