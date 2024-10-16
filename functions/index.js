927cba81f8134609dae57b5a7d12b062b1d4aa32
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-16 11:01:00 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -4,17 +4,29 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const app = express();
 
-const uri = "mongodb+srv://sudomanantial:<password>@hmvalencia2024.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
+const uri = "mongodb+srv://sudomanantial:<password>@hmvalencia2024.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"; 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors({ 
   origin: 'https://ldpalma24.github.io', 
-  methods: ['GET', 'POST'], 
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
   credentials: true // Habilita 'Access-Control-Allow-Credentials'
 }));
-
 app.use(bodyParser.json());
+
+// Middleware para habilitar CORS en todas las respuestas
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://ldpalma24.github.io'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+  next();
+});
 
 client.connect(err => {
   if (err) {
@@ -25,27 +37,4 @@ client.connect(err => {
   const collection = client.db("test").collection("encuestas");
 
   app.post('/submit-survey', async (req, res) => {
-    try {
-      const data = req.body;
-      console.log('Datos recibidos:', data);
-
-      if (Object.keys(data).length === 0) {
-        console.error('No se recibieron datos.');
-        res.status(400).json({ message: 'No se recibieron datos.' });
-        return;
-      }
-
-      await collection.insertOne(data);
-      console.log('Datos insertados en la base de datos');
-
-      res.json({ message: 'Survey submitted and data saved to Cosmos DB' });
-    } catch (error) {
-      console.error('Error en el servidor:', error);
-      res.status(500).json({ message: 'Error saving data to Cosmos DB' });
-    }
-  });
-
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-  });
-});
+    try
