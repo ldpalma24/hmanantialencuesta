358ac0552774113376f584257416cd8565b5cc5d
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-10-16 13:39:16 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -70,7 +70,9 @@ client.connect(err => {
     }
   });
 
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-  });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 });
