202c0c3f410c140bcae762b08b9b0e308c3229c9
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-11-19 12:31:09 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { Pool } = require('pg');
+
 const app = express();
 
 const pool = new Pool({
@@ -13,7 +14,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-  origin: 'https://hmanantialencuesta.vercel.app/', 
+  origin: 'https://hmanantialencuesta.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -21,40 +22,20 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://hmanantialencuesta.vercel.app/'); 
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204).end();
-  }
-  next();
-});
-
-// Cambio en la ruta para manejar las solicitudes POST
 app.post('/api/submit-survey', async (req, res) => {
   try {
     const data = req.body;
-    console.log('Datos recibidos:', data);
-
-    if (Object.keys(data).length === 0) {
-      console.error('No se recibieron datos.');
-      res.status(400).json({ message: 'No se recibieron datos.' });
-      return;
+    if (!data || Object.keys(data).length === 0) {
+      return res.status(400).json({ message: 'No se recibieron datos.' });
     }
 
     await pool.query('INSERT INTO encuestas (data) VALUES ($1)', [data]);
-    console.log('Datos insertados en la base de datos');
-
     res.json({ message: 'Survey submitted and data saved to PostgreSQL' });
   } catch (error) {
-    console.error('Error en el servidor:', error);
+    console.error('Error:', error);
     res.status(500).json({ message: 'Error saving data to PostgreSQL' });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Exporta el controlador para que Vercel lo maneje
+module.exports = app;
