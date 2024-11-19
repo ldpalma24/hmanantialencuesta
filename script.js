8e43736903c6638a5c0c0de632642ac1d27393c0
ldpalma24 <ldpalma24@users.noreply.github.com>
2024-11-19 15:14:50 +0000

    deploy: 321dce951ac18310b2e81a57011b669717f81b25

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let currentStep = 0;
-  const steps = document.querySelectorAll(".step");
-  const nextBtns = document.querySelectorAll(".next-btn");
-  const prevBtns = document.querySelectorAll(".prev-btn");
-  showStep(currentStep); // Muestra la primera sección
-
-  function showStep(stepIndex) {
-    steps.forEach((step, index) => {
-      step.classList.remove("active");
-      if (index === stepIndex) {
-        step.classList.add("active");
-      }
-    });
-    window.scrollTo(0, 0); // Desplazarse hacia arriba al cambiar de paso
-  }
-
-  function nextStep() {
-    if (currentStep < steps.length - 1) {
-      const current = steps[currentStep];
-      const next = steps[currentStep + 1];
-      current.classList.add('fall');
-      setTimeout(() => {
-        current.classList.remove('active', 'fall');
-        next.classList.add('active', 'appear');
-        setTimeout(() => {
-          next.classList.remove('appear');
-        }, 500);
-      }, 1000);
-      currentStep++;
-    }
-  }
-
-  function prevStep() {
-    if (currentStep > 0) {
-      currentStep--;
-      showStep(currentStep);
-    }
-  }
-
-  nextBtns.forEach(btn => {
-    btn.addEventListener("click", nextStep);
-  });
-
-  prevBtns.forEach(btn => {
-    btn.addEventListener("click", prevStep);
-  });
+  // (código omitido para brevedad)
 
   // Manejar el envío del formulario
   const form = document.getElementById("surveyForm");
@@ -57,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     console.log('Datos del formulario:', data);
     try {
-      const response = await fetch('https://hmanantialencuesta.vercel.app/submit-survey', { 
+      const response = await fetch('https://hmanantialencuesta.vercel.app/submit-survey', { // Asegúrate de usar la URL correcta de tu nuevo servidor
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
