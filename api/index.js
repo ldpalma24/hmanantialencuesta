67039f135a88f478a8c8d7de13343681446264d8
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-11-19 12:51:45 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,13 +5,18 @@ const { Pool } = require('pg');
 
 const app = express();
 
+const { Pool } = require('pg');
+
+// Configuración de PostgreSQL
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432
+  connectionString: process.env.DATABASE_URL, // Usa la variable de entorno DATABASE_URL
+  ssl: {
+    rejectUnauthorized: false // Railway requiere SSL, excepto en entornos internos
+  }
 });
+
+module.exports = pool;
+
 
 
 app.use(cors({
