5b1425325305f7ae3ca936962d87673c8ca02fea
ldpalma24 <ldpalma24@users.noreply.github.com>
2024-11-19 15:47:06 +0000

    deploy: ddc50f10d8740e572e2e09254d02d694e91894a5

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // (código omitido para brevedad)
+  let currentStep = 0;
+  const steps = document.querySelectorAll(".step");
+  const nextBtns = document.querySelectorAll(".next-btn");
+  const prevBtns = document.querySelectorAll(".prev-btn");
+  showStep(currentStep); // Muestra la primera sección
+
+  function showStep(stepIndex) {
+    steps.forEach((step, index) => {
+      step.classList.remove("active");
+      if (index === stepIndex) {
+        step.classList.add("active");
+      }
+    });
+    window.scrollTo(0, 0); // Desplazarse hacia arriba al cambiar de paso
+  }
+
+  function nextStep() {
+    if (currentStep < steps.length - 1) {
+      const current = steps[currentStep];
+      const next = steps[currentStep + 1];
+      current.classList.add('fall');
+      setTimeout(() => {
+        current.classList.remove('active', 'fall');
+        next.classList.add('active', 'appear');
+        setTimeout(() => {
+          next.classList.remove('appear');
+        }, 500);
+      }, 1000);
+      currentStep++;
+    }
+  }
+
+  function prevStep() {
+    if (currentStep > 0) {
+      currentStep--;
+      showStep(currentStep);
+    }
+  }
+
+  nextBtns.forEach(btn => {
+    btn.addEventListener("click", nextStep);
+  });
+
+  prevBtns.forEach(btn => {
+    btn.addEventListener("click", prevStep);
+  });
 
   // Manejar el envío del formulario
   const form = document.getElementById("surveyForm");
@@ -12,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     console.log('Datos del formulario:', data);
     try {
-      const response = await fetch('https://hmanantialencuesta.vercel.app/submit-survey', { // Asegúrate de usar la URL correcta de tu nuevo servidor
+      const response = await fetch('https://hmanantialencuesta.vercel.app//submit-survey', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
