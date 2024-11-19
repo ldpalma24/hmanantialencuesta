18c3fe5e18ba556dd88de61347c29802c3e73809
ldpalma24 <ldpalma24@users.noreply.github.com>
2024-11-19 15:50:29 +0000

    deploy: 35ae753ac37ed8a4fed403fe7fe4dc5c80fc1ff3

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     console.log('Datos del formulario:', data);
     try {
-      const response = await fetch('https://hmanantialencuesta.vercel.app//submit-survey', { 
+      const response = await fetch('https://hmanantialencuesta.vercel.app/', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
