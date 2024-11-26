a6c6c804e3d9d0bbb89de38ac8918ba7f56c752d
Lui <148378276+ldpalma24@users.noreply.github.com>
2024-11-26 20:14:16 +0000

    si

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const cors = require('cors');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(cors());
